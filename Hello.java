d51ddb9db2eca54f6c958624b33208f972d4ddd0
Marussich Sandro <sandro.marussich@generali.com>
2018-07-16 10:20:05 +0200

    prova

--- Hello.java
+++ Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		
+		System.out.println("Versione 16/07/2018-01");
 		for (int i=1;i<=5;i++) {
 			System.out.println("Hello World ... "+i);
 		}
